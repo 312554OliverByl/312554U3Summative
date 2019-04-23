0d7ea48d383347c1a601fc3719d97d9469a2e1f0
312554OliverByl <47400006+312554OliverByl@users.noreply.github.com>
2019-04-23 13:01:35 -0400

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿/* Unit 3 Summative - Hangman
+ * April 23, 2019
+ * Oliver Byl */
+using System.Windows;
+using _312554Hangman.Screens;
+
+namespace _312554Hangman
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // All the screens the game can use.
+        public MenuScreen menuScreen;
+        public PlayScreen playScreen;
+        public WinScreen winScreen;
+        public LoseScreen loseScreen;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Initialize all screens.
+            menuScreen = new MenuScreen(this);
+            playScreen = new PlayScreen(this);
+            winScreen = new WinScreen(this);
+            loseScreen = new LoseScreen(this);
+
+            // Start on the menu screen.
+            SetCurrentScreen(menuScreen);
+        }
+
+        public void SetCurrentScreen(Screen screen)
+        {
+            display.Children.Clear();
+            display.Children.Add(screen.GetCanvas());
+        }
+    }
+}
